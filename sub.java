aa1b81d43badce61cc898d4fb95e26251368d72a
Karthik <akarthik1411@gmail.com>
2025-05-31 15:55:20 -0500

    add sub to

---
+++ sub.java
@@ -0,0 +1,8 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 8;
+        int result = a - b;
+
+        System.out.println("The result of subtraction is: " + result);
+    }
